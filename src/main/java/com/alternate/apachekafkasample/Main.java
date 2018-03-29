e4b0847b08fb6c723e469e2cb0d75ee04ecdac3f
randilfernando <randil.fernando@outlook.com>
2018-03-29 06:15:39 +0530

    added sample kafka consumer to test ibuy event emitting

---
+++ src/main/java/com/alternate/apachekafkasample/Main.java
@@ -0,0 +1,27 @@
+package com.alternate.apachekafkasample;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("order-created", "item-added", "order-finalized"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("order-created: %s\n", record.value());
+        }
+    }
+}
